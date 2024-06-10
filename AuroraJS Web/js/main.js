9fb58090ca072f0fa205e4a34cb162df66babee6
stormey2010 <72316379+gamesaver2010@users.noreply.github.com>
2024-06-09 21:45:31 -0700

    Add files via upload

---
+++ AuroraJS Web/js/main.js
@@ -0,0 +1,7 @@
+// Get the dropdown button
+var dropdownBtn = document.querySelector('.download-button-hero');
+
+// Toggle the dropdown on click
+dropdownBtn.addEventListener('click', function() {
+    dropdownBtn.classList.toggle('active');
+});
